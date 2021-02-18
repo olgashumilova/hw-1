1b7ba23556defb1b045232c53bd968fd4406bceb
olgashumilova <volga_shumilova@mail.ru>
2021-02-18 22:16:19 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const data1 = [
+    {
+      "age": 3,
+      "name": "Curtis Greene",
+      "email": "curtis.greene@zaj.ca",
+    },
+    {
+      "age": 23,
+      "name": "Nikki Lowe",
+      "email": "nikki.lowe@darwinium.tv",
+    },
+    {
+      "age": 39,
+      "name": "Barr Copeland",
+      "email": "barr.copeland@nipaz.me",
+    },
+  ];
+  const data2 = [
+    {
+      "age": 29,
+      "name": "Calhoun Woodward",
+      "email": "calhoun.woodward@medmex.info",
+    },
+    {
+      "age": 21,
+      "name": "Leta Lee",
+      "email": "leta.lee@qnekt.com",
+    },
+    {
+      "age": 40,
+      "name": "James Dinh",
+      "email": "j.dink@erw.com",
+    }
+  ];
+  
